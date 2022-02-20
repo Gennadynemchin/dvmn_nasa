f4449d68b332aac78690a31287e1d8e4ad731483
Gennadynemchin <69091759+Gennadynemchin@users.noreply.github.com>
2022-02-20 20:15:07 +0700

    Added get_last_spacex()

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import requests
-from pathlib import Path
+import json
 import os
 from urllib.parse import urlparse
 from requests.models import HTTPError
-
+from pathlib import Path
 
 def get_pic(url_pic, destination_folder):
     Path(destination_folder).mkdir(parents=True, exist_ok=True)
@@ -16,10 +16,28 @@ def get_pic(url_pic, destination_folder):
     return response.ok
 
 
-def main():
-        get_pic('https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg', 'images')
+def get_last_spacex(destination_folder):
+    Path(destination_folder).mkdir(parents=True, exist_ok=True)
+    spacex_launch_counter = 1
+    filename = f'{destination_folder}/spacex_launch_{spacex_launch_counter}.jpeg'
+    response = requests.get('https://api.spacexdata.com/v3/launches/past')
+    response.raise_for_status()
+    decoded_response = response.json()
+    last_launch_pics = decoded_response[-{spacex_launch_counter}]['links']['flickr_images']
 
+    while len(last_launch_pics) == 0:
+
+        '''
+        if len(last_launch_pics) > 0:
+            for x in last_launch_pics:
+                with open(filename, 'wb') as file:
+                    file.write(x)
+        return response.ok
+        '''
+
+def main():
+    #get_pic('https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg', 'images')
+    get_last_spacex('spacex')
 
 if __name__ == '__main__':
     main()
-
