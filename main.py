9bdfa2224bb6f9c58efc3ec0ea74c96efcab8122
Gennadynemchin <69091759+Gennadynemchin@users.noreply.github.com>
2022-02-20 16:37:33 +0700

    Added Path().mkdir() to the get_pic() func

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import requests
+from pathlib import Path
 import os
 from urllib.parse import urlparse
 from requests.models import HTTPError
 
 
 def get_pic():
-    filename = 'hubble.jpeg'
+    Path("images").mkdir(parents=True, exist_ok=True)
+    filename = 'images/hubble.jpeg'
     url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
     response = requests.get(url)
     response.raise_for_status()
