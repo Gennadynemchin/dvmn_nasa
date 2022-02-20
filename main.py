b5ceaa523fce7eea545c4c61d4b867fc724a0bb0
Gennadynemchin <69091759+Gennadynemchin@users.noreply.github.com>
2022-02-20 20:24:34 +0700

    Added get_last_spacex()

--- main.py
+++ main.py
@@ -18,22 +18,20 @@ def get_pic(url_pic, destination_folder):
 
 def get_last_spacex(destination_folder):
     Path(destination_folder).mkdir(parents=True, exist_ok=True)
-    spacex_launch_counter = 1
+    spacex_launch_counter = -1
     filename = f'{destination_folder}/spacex_launch_{spacex_launch_counter}.jpeg'
     response = requests.get('https://api.spacexdata.com/v3/launches/past')
     response.raise_for_status()
     decoded_response = response.json()
-    last_launch_pics = decoded_response[-{spacex_launch_counter}]['links']['flickr_images']
+    last_launch_pics = decoded_response[spacex_launch_counter]['links']['flickr_images']
 
-    while len(last_launch_pics) == 0:
-
-        '''
-        if len(last_launch_pics) > 0:
-            for x in last_launch_pics:
-                with open(filename, 'wb') as file:
-                    file.write(x)
-        return response.ok
-        '''
+    if len(last_launch_pics) > 0:
+        for x in last_launch_pics:
+            with open(filename, 'wb') as file:
+                file.write(x)
+    else:
+        spacex_launch_counter -= 1
+    return response.ok
 
 def main():
     #get_pic('https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg', 'images')
