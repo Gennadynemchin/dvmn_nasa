9da194087f6e511db5d3a37e7e156b20ed606e4a
Gennadynemchin <69091759+Gennadynemchin@users.noreply.github.com>
2022-02-26 00:00:57 +0700

    Added nasa_apod() func;

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import datetime
 import requests
 import json
 import os
+from dotenv import load_dotenv
 from urllib.parse import urlparse
 from requests.models import HTTPError
 from pathlib import Path
@@ -44,9 +45,22 @@ def fetch_spacex_last_launch(destination_folder):
     return response.ok
 
 
+def nasa_apod(destination_folder, nasa_token):
+    Path(destination_folder).mkdir(parents=True, exist_ok=True)
+    params = {'api_key' : nasa_token}
+    response = requests.get('https://api.nasa.gov/planetary/apod', params=params)
+    response.raise_for_status()
+    decoded_response = response.json()
+
+    return decoded_response
+
+
 def main():
+    load_dotenv()
+    nasa_token = os.getenv('NASATOKEN')
     #get_pic('https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg', 'images')
-    fetch_spacex_last_launch('spacex')
+    #fetch_spacex_last_launch('spacex')
+    print(nasa_apod('nasa_apod', nasa_token))
 
 if __name__ == '__main__':
     main()
