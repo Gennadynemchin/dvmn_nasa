33f950c20117257ed819255a113047d033c1d7fc
Gennadynemchin <69091759+Gennadynemchin@users.noreply.github.com>
2022-03-12 18:24:04 +0300

    Fixed fetch_spacex.py - len(array) been changed by 'for' cycle;

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -3,25 +3,18 @@ import datetime
 from pathlib import Path
 
 
-def fetch_spacex_last_launch(destination_folder):
+def get_spacex_last_launch(destination_folder):
     Path(destination_folder).mkdir(parents=True, exist_ok=True)
-    spacex_launch_counter = -1
     response = requests.get('https://api.spacexdata.com/v3/launches/past')
     response.raise_for_status()
     decoded_response = response.json()
-    last_launch_pics = decoded_response[spacex_launch_counter]['links']['flickr_images']
 
-    while len(last_launch_pics) < 1:
-        spacex_launch_counter -= 1
-        last_launch_pics = decoded_response[spacex_launch_counter]['links']['flickr_images']
-
-    Path(destination_folder).mkdir(parents=True, exist_ok=True)
-
-    for x in last_launch_pics:
-        filename = f'{destination_folder}/spacex_launch_{datetime.datetime.now().time()}.jpeg'
-        response = requests.get(x)
-        response.raise_for_status()
-
-        with open(filename, 'wb') as file:
-            file.write(response.content)
-    return response.ok
+    for launch in reversed(decoded_response):
+        if launch['links']['flickr_images']:
+            for to_download in launch['links']['flickr_images']:
+                filename = f'{destination_folder}/spacex_launch_{datetime.datetime.now().time()}.jpeg'
+                response = requests.get(to_download)
+                response.raise_for_status()
+                with open(filename, 'wb') as file:
+                    file.write(response.content)
+            break
