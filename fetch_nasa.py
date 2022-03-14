fc7b88e0fcc1ce1f58fd72ab7b0fb2a4254a4c0d
Gennadynemchin <69091759+Gennadynemchin@users.noreply.github.com>
2022-03-14 22:38:57 +0300

    fetch_nasa.py: deleted try-except;

--- fetch_nasa.py
+++ fetch_nasa.py
@@ -17,7 +17,6 @@ def get_nasa_apod(destination_folder, nasa_token):
             apod_link = to_download.get('hdurl')
 
             filename = posixpath.basename(urllib.parse.unquote(apod_link))
-            #extension = os.path.splitext(filename)[-1]
             response = requests.get(apod_link)
             response.raise_for_status()
             file_path = f'{destination_folder}/nasa_apod_{filename}'
@@ -38,19 +37,15 @@ def get_nasa_epic(destination_folder, nasa_token):
     decoded_response = response.json()
 
     for to_download in decoded_response:
-        try:
-            image = to_download.get('image')
-            date_time_str = to_download.get('date')
-            date_time_obj = datetime.datetime.strptime(date_time_str, '%Y-%m-%d %H:%M:%S')
-            year, month, day = date_time_obj.year, '%02d' % date_time_obj.month, '%02d' % date_time_obj.day
-            urlpath = f'https://api.nasa.gov/EPIC/archive/natural/{year}/{month}/{day}/png/{image}.png'
-            filename = posixpath.basename(urllib.parse.unquote(urlpath))
-            response_pic = requests.get(urlpath, params=params)
-            response_pic.raise_for_status()
-            file_path = f'{destination_folder}/nasa_epic_{filename}'
-        except TypeError:
-            pass
-        else:
-            with open(file_path, 'wb') as file:
-                file.write(response_pic.content)
+        image = to_download.get('image')
+        date_time_str = to_download.get('date')
+        date_time_obj = datetime.datetime.strptime(date_time_str, '%Y-%m-%d %H:%M:%S')
+        year, month, day = date_time_obj.year, '%02d' % date_time_obj.month, '%02d' % date_time_obj.day
+        urlpath = f'https://api.nasa.gov/EPIC/archive/natural/{year}/{month}/{day}/png/{image}.png'
+        filename = posixpath.basename(urllib.parse.unquote(urlpath))
+        response_pic = requests.get(urlpath, params=params)
+        response_pic.raise_for_status()
+        file_path = f'{destination_folder}/nasa_epic_{filename}'
+        with open(file_path, 'wb') as file:
+            file.write(response_pic.content)
     return response.ok
